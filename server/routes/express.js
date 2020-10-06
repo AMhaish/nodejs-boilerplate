a27d20543446291b653a6d728342cdfbc705a1a5
Ahmad Mhaish <a.mhaish@gmail.com>
2020-10-06 11:08:20 +0300

    Removing unneeded code

--- server/routes/express.js
+++ server/routes/express.js
@@ -4,8 +4,5 @@ const helpers = require('./helpers');
 module.exports = (app, ioc, logger) => {
     const recordsController = ioc.resolve(Controllers.RecordsController);
     app.post("/records/summary", async (req, res, next) => { await helpers.handleException(req, res, next, recordsController.getRecordsSummary) });
-    app.get('*', function (req, res) {
-        res.status(404).send({ error: 'Not found' });
-    });
     logger.logInfo(`Running an Express API server at /records/*`, 'Express routes');
 };
